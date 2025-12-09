c33c3c1a05082f4e30b6e7fb2a317039c0d26456
suggit <creek.frogs@gmail.com>
2025-12-09 00:33:59 -0800

    add file arg

--- sjon.c
+++ sjon.c
@@ -1,57 +1,77 @@
+#include <stdio.h>
+
 #include <SDL2/SDL.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Surface* psurface = nullptr;
-	SDL_Window* pwin = nullptr;
-
-	int width = 900;
-	int height = 600;
-
-	pwin = SDL_CreateWindow(
-			"sjon",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			width,
-			height,
-			0);
-
-	psurface = SDL_GetWindowSurface(pwin);
-
-	SDL_Rect pixel;
-	pixel.x = 0;
-	pixel.y = 0;
-	pixel.w = 1;
-	pixel.h = 1;
-	for(int x=0; x < width; x++)
+	if(argc < 1)
 	{
-		pixel.x = x;
-		for(int y=0; y < height; y++)
-		{
-			pixel.y = y;
-			SDL_FillRect(
-				psurface,
-				&pixel,
-				SDL_MapRGB(psurface->format, 255, 0, 0));
-				
-		}
+		printf("incorrect usage.");
+		return 0;
 	}
 
-	SDL_UpdateWindowSurface(pwin);
-
-	bool running = 1;
-	while(running)
+	for (int i = 1; i < argc; i++)
 	{
-		SDL_Event e;
-		while(SDL_PollEvent(&e))
-		{
-			if(e.type == SDL_QUIT)
-				running = 0;
-		}
-		SDL_Delay(16);
-	}
+		const char *filename = argv[i];
+		FILE *file = fopen(filename, "rb");
 
-	SDL_DestroyWindow(pwin);
-	SDL_Quit();
+		if(!file)
+		{
+			printf("error reading file");
+			return 0;
+		}
+
+		SDL_Surface* psurface = nullptr;
+		SDL_Window* pwin = nullptr;
+
+		int width = 900;
+		int height = 600;
+
+		pwin = SDL_CreateWindow(
+				"sjon",
+				SDL_WINDOWPOS_CENTERED,
+				SDL_WINDOWPOS_CENTERED,
+				width,
+				height,
+				0);
+
+		psurface = SDL_GetWindowSurface(pwin);
+
+		SDL_Rect pixel;
+		pixel.x = 0;
+		pixel.y = 0;
+		pixel.w = 1;
+		pixel.h = 1;
+		for(int x=0; x < width; x++)
+		{
+			pixel.x = x;
+			for(int y=0; y < height; y++)
+			{
+				pixel.y = y;
+				SDL_FillRect(
+					psurface,
+					&pixel,
+					SDL_MapRGB(psurface->format, 255, 0, 0));
+					
+			}
+		}
+
+		SDL_UpdateWindowSurface(pwin);
+
+		bool running = 1;
+		while(running)
+		{
+			SDL_Event e;
+			while(SDL_PollEvent(&e))
+			{
+				if(e.type == SDL_QUIT)
+					running = 0;
+			}
+			SDL_Delay(16);
+		}
+
+		SDL_DestroyWindow(pwin);
+		SDL_Quit();
+	}
 }
