999bf867b005c8db64d487397b6d26ffc5487ecf
ovah <admin@ovah.dev>
2025-12-08 23:13:38 -0800

    event loop

--- sjon.c
+++ sjon.c
@@ -3,14 +3,14 @@
 int main()
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Surface* psurface = 0;
-	SDL_Window* pwin = 0;
+	SDL_Surface* psurface = nullptr;
+	SDL_Window* pwin = nullptr;
 
-	const char* wtitle = "sjon";
 	int width = 900;
 	int height = 600;
 
-	pwin = SDL_CreateWindow(wtitle,
+	pwin = SDL_CreateWindow(
+			"sjon",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
 			width,
@@ -19,10 +19,25 @@ int main()
 
 	psurface = SDL_GetWindowSurface(pwin);
 
-	SDL_FillRect(psurface,
-			0,
+	SDL_FillRect(
+			psurface,
+			nullptr,
 			SDL_MapRGB(psurface->format, 0, 0, 0));
 	
 	SDL_UpdateWindowSurface(pwin);
-	SDL_Delay(3000);
+
+	bool running = 1;
+	while(running)
+	{
+		SDL_Event e;
+		while(SDL_PollEvent(&e))
+		{
+			if(e.type == SDL_QUIT)
+				running = 0;
+		}
+		SDL_Delay(16);
+	}
+
+	SDL_DestroyWindow(pwin);
+	SDL_Quit();
 }
